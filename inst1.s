46c2d2dae228a7678e4c068d99c2d94c3c3911cc
Talesrib <talespiano@gmail.com>
2022-12-20 18:43:35 -0300

    adiciona problema 1 (fatorial)

--- inst1.s
+++ inst1.s
@@ -1,16 +1,14 @@
 .text
 .globl main
 main:
-	addi x1, zero , 5 # n
-	addi x5,zero, -1 #contante com valor -1
-    addi x3, zero, 1 #contante com valor 1
-    addi x4, zero, 1 #x4 -e o valor final de fatorial (valor inicial um)
-    add x6, zero, x1
-    j fact
-fact:
-    bge x6,x3, maiorUm # se for maior que um
-    
-maiorUm:
-	add x7, x1, x5
-	mul x4,x6,x7
-    j fact
+	li a0, 1
+    li a1, 1
+    addi a2, zero, 5
+    beq a2, zero, fim
+loop:
+	mul a1, a1, a2
+    sub a2, a2, a0
+    beq a2, a0, fim
+    bgt a2, a0, loop
+fim:
+	nop
